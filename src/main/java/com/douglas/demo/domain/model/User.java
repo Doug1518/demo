6cd34bf7624f72762d7e60e4fd2b99e1e183a047
Douglas Augusto <149178392+Doug1518@users.noreply.github.com>
2025-05-02 18:02:38 -0300

    Usando o lombok
    retirada de código boilerplate  com lombok para deixar o código mais limpo

--- src/main/java/com/douglas/demo/domain/model/User.java
+++ src/main/java/com/douglas/demo/domain/model/User.java
@@ -10,7 +10,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import lombok.Data;
 
+@Data
 @Entity(name = "tb_user")
 public class User {
     
@@ -31,53 +33,5 @@ public class User {
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<News> news;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Account getAccount() {
-        return account;
-    }
-
-    public void setAccount(Account account) {
-        this.account = account;
-    }
-
-    public Card getCard() {
-        return card;
-    }
-
-    public void setCard(Card card) {
-        this.card = card;
-    }
-
-    public List<Feature> getFeatures() {
-        return features;
-    }
-
-    public void setFeatures(List<Feature> features) {
-        this.features = features;
-    }
-
-    public List<News> getNews() {
-        return news;
-    }
-
-    public void setNews(List<News> news) {
-        this.news = news;
-    }
 
 }
