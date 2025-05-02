da6fba70ab00dd13bbd0a5a816bc350f57568d7e
Douglas Augusto <149178392+Doug1518@users.noreply.github.com>
2025-05-02 17:56:53 -0300

    criação da api
    criação de entidades, controllers e outras

---
+++ src/main/java/com/douglas/demo/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.douglas.demo.domain.repository;
+
+import com.douglas.demo.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
---
+++ src/main/java/com/douglas/demo/service/impl/UserService.java
@@ -0,0 +1,8 @@
+package com.douglas.demo.service.impl;
+
+
+import com.douglas.demo.domain.model.User;
+
+public interface UserService extends CrudService<Long, User> {
+
+}
